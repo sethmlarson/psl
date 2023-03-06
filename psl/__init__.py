7b6661462e27ad9a9b22c69ca89f9c812b20134f
Seth Michael Larson <sethmichaellarson@gmail.com>
2023-03-06 13:09:03 +0000

    Automated updates to the Public Suffix list on 2023-3-6

--- psl/__init__.py
+++ psl/__init__.py
@@ -2,8 +2,8 @@ import functools
 import pathlib
 import typing
 
-__version__ = "2023.2.27"
-__checksum__ = "02f65eca4f16cfd3fb3c3a94eeb4a57a00fe558a"
+__version__ = "2023.3.6"
+__checksum__ = "b72f10dace8f91f397e8b0c7b7c4ea8ef1603a16"
 __all__ = ["PUBLIC_SUFFIX_URL", "domain_suffixes", "Suffixes", "domain_can_set_cookie"]
 
 
